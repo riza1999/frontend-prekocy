c334686ff6f010d08a82f499041ed0fe435605fc
rizakurniawanda <rizakurniawan48@gmail.com>
2021-09-18 13:42:28 +0700

    update

--- application/views/file_view.php
+++ application/views/file_view.php
@@ -6,11 +6,11 @@
                 <div class="col-xl-8 pt-5">
                     <?php echo form_open_multipart('Prediksi/importExcel'); ?>
                     <div class="h2 fs-1 text-black mb-5 text-center">Masukkan komentar yang ingin diprediksi!</div>
-                    <button class="btn btn-primary mx-auto d-block rounded-pill px-4" id="btnDownload" >Download</button>
+                    <button class="btn btn-primary mx-auto d-block rounded-pill px-4 mb-4" id="btnDownload" ><i class="bi bi-arrow-down"></i>Download Template Excel</button>
                     <div class="input-group mb-4 px-5">
                         <input id="inputFile" type="file" class="form-control rounded-pill" name="upload_file">
                     </div>
-                    <button type="submit" class="btn btn-primary mx-auto d-block rounded-pill px-4" id="btnUpload" >Upload</button>
+                    <button type="submit" class="btn btn-primary mx-auto d-block rounded-pill px-4" id="btnUpload"><i class="bi bi-arrow-up"></i>Upload</button>
                     <?php echo form_close(); ?>
                 </div>
             </div>
